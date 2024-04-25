2878307e7f3ff7ac7a4e89853a19b1eb289cc9eb
Tung Nguyen <tungnt620@gmail.com>
2024-04-25 22:14:51 +0700

    add typing

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -6,4 +6,5 @@ app_name = "flashcard"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:fcs_id>/", views.fcs_detail, name="fcs_detail"),
+    path("fc/<int:fc_id>/", views.FlashcardDetailView.as_view(), name="fc_detail"),
 ]
--- flashcard/views.py
+++ flashcard/views.py
@@ -1,10 +1,18 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import QuerySet
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
 from flashcard.models import FlashcardSet, Flashcard
 
 
-def index(request):
-    your_fcs_list = FlashcardSet.objects.filter(created_by=request.user)
+def index(request: WSGIRequest):
+    your_fcs_list: QuerySet[FlashcardSet]
+
+    if request.user.is_anonymous:
+        your_fcs_list = QuerySet[FlashcardSet]()
+    else:
+        your_fcs_list = FlashcardSet.objects.filter(created_by=request.user)
 
     return render(
         request,
@@ -15,13 +23,25 @@ def index(request):
     )
 
 
-def fcs_detail(request, fcs_id: int):
-    fcs = get_object_or_404(FlashcardSet, pk=fcs_id)
+def fcs_detail(request: WSGIRequest, fcs_id: int):
+    fcs: FlashcardSet = get_object_or_404(FlashcardSet, pk=fcs_id)
+    list_fc: QuerySet[Flashcard] = Flashcard.objects.filter(flashcard_set=fcs)
 
     return render(
         request,
         "flashcard/fcs_detail.html",
         {
             "fcs": fcs,
+            "list_fc": list_fc,
         },
     )
+
+
+class FlashcardDetailView(generic.DetailView):
+    model = Flashcard
+    template_name = "flashcard/fc_detail.html"
+    context_object_name = "flashcard"
+    pk_url_kwarg = "fc_id"
+
+    def get_queryset(self):
+        return Flashcard.objects.filter(created_by=self.request.user)
